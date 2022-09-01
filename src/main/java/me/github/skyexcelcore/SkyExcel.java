2ac4d6e5097d9d785494e5e94de0b957efafc3fe
BuildTools <unconfigured@null.spigotmc.org>
2022-09-02 06:39:15 +0900

    Core 업데이트

--- src/main/java/me/github/skyexcelcore/SkyExcel.java
+++ src/main/java/me/github/skyexcelcore/SkyExcel.java
@@ -6,6 +6,9 @@ import me.github.skyexcelcore.annotation.Adjust;
 import me.github.skyexcelcore.annotation.Registerclass;
 import me.github.skyexcelcore.command.abcd;
 import me.github.skyexcelcore.command.test;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.reflections.Reflections;
 
@@ -13,6 +16,9 @@ import org.reflections.Reflections;
 public class SkyExcel extends JavaPlugin {
 
     public static SkyExcel plugin;
+
+    public static Plugin newPlugin;
+
 
     @Override
     public void onEnable() {
@@ -36,12 +42,24 @@ public class SkyExcel extends JavaPlugin {
 //
 
 
-
     }
 
+    public static void setNewPlugin(Plugin newPlugin) {
+        SkyExcel.newPlugin = newPlugin;
+    }
+
+    public static Plugin getNewPlugin() {
+        return newPlugin;
+    }
 
     public static SkyExcel getPlugin() {
         return plugin;
     }
+
+    public static void RegisterEvents(Plugin plugin, Listener listener) {
+        PluginManager pm = plugin.getServer().getPluginManager();
+        pm.registerEvents(listener, plugin);
+    }
+
 
 }
