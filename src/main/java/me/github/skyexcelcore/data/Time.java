3eb9e283a2ed3a46e04a4b3b1594e5745fbdaff4
BuildTools <unconfigured@null.spigotmc.org>
2022-09-01 16:47:42 +0900

    TimeLib 추가

--- src/main/java/me/github/skyexcelcore/data/Time.java
+++ src/main/java/me/github/skyexcelcore/data/Time.java
@@ -5,5 +5,103 @@ import org.bukkit.event.Listener;
 public class Time implements Listener {
 
 
+    private long SECOND_IN_MILLIS = 0;
 
+    private long MINUTE_IN_MILLIS ;
+    private long HOUR_IN_MILLIS;
+    private long DAY_IN_MILLIS;
+
+    public Time(int... times) {
+        switch (times.length) {
+            case 1:
+                addDay(times[0]);
+                break;
+            case 2:
+                addDay(times[0]);
+                addHour(times[1]);
+                break;
+            case 3:
+                addDay(times[0]);
+                addHour(times[1]);
+                addMinute(times[2]);
+                break;
+            case 4:
+                addDay(times[0]);
+                addHour(times[1]);
+                addMinute(times[2]);
+                addSecond(times[3]);
+                break;
+        }
+    }
+
+    public long addDay(long days) {
+        long milliseconds = days * 24 * 60 * 60 * 1000;
+        SECOND_IN_MILLIS = milliseconds + SECOND_IN_MILLIS;
+        return SECOND_IN_MILLIS;
+    }
+
+    public long addHour(long hour) {
+        long milliseconds = hour * 60 * 60 * 1000;
+        SECOND_IN_MILLIS = milliseconds + SECOND_IN_MILLIS;
+        return SECOND_IN_MILLIS;
+    }
+
+    public long addMinute(long min) {
+        long milliseconds = min * 60 * 1000;
+        SECOND_IN_MILLIS = milliseconds + SECOND_IN_MILLIS;
+        return SECOND_IN_MILLIS;
+    }
+
+    public long addSecond(long second) {
+        long milliseconds = second * 1000;
+        SECOND_IN_MILLIS = milliseconds + SECOND_IN_MILLIS;
+        return SECOND_IN_MILLIS;
+    }
+
+
+    public long minDay(long days) {
+        long milliseconds = days * 24 * 60 * 60 * 1000;
+        SECOND_IN_MILLIS = SECOND_IN_MILLIS - milliseconds;
+        return SECOND_IN_MILLIS;
+    }
+
+    public long minHour(long hour) {
+        long milliseconds = hour * 60 * 60 * 1000;
+        SECOND_IN_MILLIS = SECOND_IN_MILLIS - milliseconds;
+        return SECOND_IN_MILLIS;
+    }
+
+    public long minMinute(long min) {
+        long milliseconds = min * 60 * 1000;
+        SECOND_IN_MILLIS = SECOND_IN_MILLIS - milliseconds;
+        return SECOND_IN_MILLIS;
+    }
+
+    public long minSecond(long min) {
+        long milliseconds = min * 1000;
+        SECOND_IN_MILLIS = SECOND_IN_MILLIS - milliseconds;
+        return SECOND_IN_MILLIS;
+    }
+
+    public void setMillSecond(long SECOND_IN_MILLIS) {
+        this.SECOND_IN_MILLIS = SECOND_IN_MILLIS;
+    }
+
+    public long DAY() {
+        return ((SECOND_IN_MILLIS / (1000 * 60 * 60 * 24)));
+    }
+    public long HOUR() {
+        return ((SECOND_IN_MILLIS / (1000 * 60 * 60)) % 24);
+    }
+
+    public long MINUTE() {
+        return ((SECOND_IN_MILLIS / (1000 * 60)) % 60);
+    }
+
+    public long SECOND() {
+        return SECOND_IN_MILLIS / (1000) % 60;
+    }
+    public long SECOND_IN_MILLIS() {
+        return SECOND_IN_MILLIS;
+    }
 }
